8d935613d9f228d73f76566fec78696c4132fbb0
Sumaiyashe <sumaiyashompa137@gmail.com>
2022-04-06 00:05:43 +0600

    blogs element push

---
+++ src/Components/Blogs/Blogs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Blogs.css';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h2>Blogs</h2>
+            <h1>1. Purpose of Context API?</h1>
+            <p></p>
+            <h1>2. Semantic Tag?</h1>
+            <p></p>
+            <h1>3. Difference between Inline Block and Inline Elements?</h1>
+            <p></p>
+        </div>
+    );
+};
+
+export default Blogs;
